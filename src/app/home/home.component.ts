c6741d78ecc3ef2e376251c10a79e89442eba19a
nimaz niyas <mgha123@gmail.com>
2018-12-05 12:27:50 +0300

    added animation stuff, didn't work. Remove item on click of item

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('goalsAnimater', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+        query('enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35px)', offset: .3 }),
+            style({ opacity: 1, transform: 'translateY(0)', offset: 1 })
+          ]))
+        ]), { optional: true })
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   itemCount: Number;
@@ -29,6 +44,11 @@ export class HomeComponent implements OnInit {
       this.itemCount = this.todoList.length;
       this.inputText = '';
     }
+  }
+
+
+  removeItem(index) {
+    this.todoList.splice(index, 1)
   }
 
 }
